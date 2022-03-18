5cb90cc89a6412f530e5bd5bb4bba17dae46fcfa
Andrea Carraro <me@andreacarraro.it>
2022-03-18 18:05:13 +0100

    feat: expose an inspector factory

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # MSW inspector
+
+## How to
+
+```js
+import { createMSWInspector } from 'msw-inspector';
+import { server } from '@/mocks/server';
+const mswInspector = createMSWInspector({
+  server,
+});
+
+beforeAll(() => {
+  mswInspector.setup();
+});
+
+beforeEach(() => {
+  mswInspector.clear();
+});
+
+afterAll(() => {
+  mswInspector.teardown();
+});
+
+describe('My test', () => {
+  it('My test', async () => {
+    // Perform your test preparation
+
+    expect(
+      mswInspector.getCalls(`/expected/network/request/path`)
+    ).toHaveBeenCalledWith({
+      method: 'GET',
+      headers: {
+        'my-header': 'value',
+      },
+      body: {
+        'my-body': 'value',
+      },
+      query: {
+        'my-query': 'value',
+      },
+    });
+  });
+});
+```
--- src/index.ts
+++ src/index.ts
@@ -8,86 +8,95 @@ type RequestLogRecord = {
   query?: Record<string, string>;
 };
 
+// @NOTE This is the only touch point with Jest
+// @TODO Let user provide it's own framework mock function
 type RequestLogEntry = jest.Mock<void, [RequestLogRecord]>;
 
-// Store network requests by path
-const requestLog = new Map<string, RequestLogEntry>();
+/**
+ * Create a new MSW inspector instance bound to the provided msw server setup
+ */
+function createMSWInspector({ server }: { server: SetupServerApi }) {
+  // Store network requests by path
+  // @TODO store absolute urls, path names or both?
+  const requestLog = new Map<string, RequestLogEntry>();
 
-function logRequest(req: MockedRequest): void {
-  const { method, headers, body } = req;
-  const { pathname, searchParams } = req.url;
+  function logRequest(req: MockedRequest): void {
+    const { method, headers, body } = req;
+    const { pathname, searchParams } = req.url;
 
-  const query =
-    Array.from(searchParams.keys()).length > 0
-      ? Object.fromEntries(searchParams)
-      : undefined;
+    const query =
+      Array.from(searchParams.keys()).length > 0
+        ? Object.fromEntries(searchParams)
+        : undefined;
 
-  // Create an inspectionable RequestLogRecord object and store it in requestLog map
-  if (pathname) {
-    // Create a new request log entry (Jest's mock function) for current pathname, if necessary
-    if (!requestLog.has(pathname)) {
-      const newRequestLogEntry: RequestLogEntry = jest.fn();
-      requestLog.set(pathname, newRequestLogEntry);
-    }
+    // Create an inspectionable RequestLogRecord object and store it in requestLog map
+    if (pathname) {
+      // Create a new request log entry (Jest's mock function) for current pathname, if necessary
+      if (!requestLog.has(pathname)) {
+        const newRequestLogEntry: RequestLogEntry = jest.fn();
+        requestLog.set(pathname, newRequestLogEntry);
+      }
 
-    const requestLogEntry = requestLog.get(pathname);
-    if (requestLogEntry) {
-      const requestLogRecord: RequestLogRecord = {
-        method,
-        headers: headers.all(),
-        ...(body && { body }),
-        ...(query && { query }),
-      };
+      const requestLogEntry = requestLog.get(pathname);
+      if (requestLogEntry) {
+        const requestLogRecord: RequestLogRecord = {
+          method,
+          headers: headers.all(),
+          ...(body && { body }),
+          ...(query && { query }),
+        };
 
-      requestLogEntry(requestLogRecord);
+        requestLogEntry(requestLogRecord);
+      }
     }
   }
+
+  return {
+    /**
+     * Return a Jest mock function holding a RequestLogRecord for each call performed against provided path.
+     * Network requested are spied through msw listeners
+     *
+     * @param {string} path Path of a network request (`/path`)
+     * @return {*} {jest.Mock}
+     */
+    getCalls(path: string): jest.Mock {
+      const requestLogEntry = requestLog.get(path);
+      if (requestLogEntry) {
+        return requestLogEntry;
+      }
+
+      const availablePaths = Array.from(requestLog.keys());
+      throw new Error(
+        `[msw-inspector] Cannot find a matching requests for path: ${path}. Intercepted requests paths are:\n\n${availablePaths.join(
+          '\n'
+        )}`
+      );
+    },
+
+    /**
+     * Setup a msw spy. Call it before tests are executed
+     */
+    setup() {
+      this.clear();
+      // https://mswjs.io/docs/extensions/life-cycle-events#methods
+      server.events.on('request:start', logRequest);
+    },
+
+    /**
+     * Clear msw spy call log. Call it before every single test execution
+     */
+    clear() {
+      requestLog.clear();
+    },
+
+    /**
+     * Tear down msw spy. Call it after all tests are executed
+     */
+    teardown() {
+      this.clear();
+      server.events.removeListener('request:start', logRequest);
+    },
+  };
 }
 
-const spyNetworkRequests = {
-  /**
-   * Return a Jest mock function holding a RequestLogRecord for each call performed against provided path.
-   * Network requested are spied through msw listeners
-   *
-   * @param {string} path Path of a network request (/path)
-   * @return {*} {jest.Mock}
-   */
-  getCalls(path: string): jest.Mock {
-    const requestLogEntry = requestLog.get(path);
-    if (requestLogEntry) {
-      return requestLogEntry;
-    }
-
-    const availablePaths = Array.from(requestLog.keys());
-    throw new Error(
-      `[msw-inspector] Cannot find a matching requests for path: ${path}. Intercepted requests paths are:\n\n${availablePaths.join(
-        '\n'
-      )}`
-    );
-  },
-
-  /**
-   * Setup a msw spy. Call it before tests are executed
-   */
-  setup({ server }: { server: SetupServerApi }) {
-    this.clear();
-    server.events.on('request:start', logRequest);
-  },
-
-  /**
-   * Clear msw spy call log. Call it before every single test execution
-   */
-  clear() {
-    requestLog.clear();
-  },
-
-  /**
-   * Tear down msw spy. Call it after all tests are executed
-   */
-  teardown({ server }: { server: SetupServerApi }) {
-    this.clear();
-    server.events.removeListener('request:start', logRequest);
-  },
-};
-
-export default spyNetworkRequests;
+export { createMSWInspector };
