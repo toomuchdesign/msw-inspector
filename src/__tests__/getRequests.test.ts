6b11538982180c6f4bc37727e66015de61eac9cf
Andrea Carraro <me@andreacarraro.it>
2023-07-08 00:31:00 +0200

    test: call order

--- src/__tests__/getRequests.test.ts
+++ src/__tests__/getRequests.test.ts
@@ -32,11 +32,62 @@ describe('getRequests', () => {
       });
     });
 
+    describe('multiple matching calls', () => {
+      it('preserves calls order', async () => {
+        await fetch('http://origin.com', { headers: { id: 'first' } });
+        await fetch('http://origin.com', { headers: { id: 'second' } });
+        await fetch('http://origin.com', { headers: { id: 'third' } });
+
+        const matchingCalls = mswInspector.getRequests('http://origin.com');
+        expect(matchingCalls).toHaveBeenCalledTimes(3);
+
+        ['first', 'second', 'third'].forEach((id, index) => {
+          expect(matchingCalls).toHaveBeenNthCalledWith(index + 1, {
+            method: 'GET',
+            headers: {
+              id,
+            },
+          });
+        });
+      });
+    });
+
     describe('url with port', () => {
       it('returns requests', async () => {
         const path = 'http://origin.com:1234/path/param';
         await fetch(path);
         expect(mswInspector.getRequests(path)).toHaveBeenCalledTimes(1);
+      });
+    });
+
+    describe('matching patterns', () => {
+      describe('multiple :namedParams', () => {
+        it('find matching request', async () => {
+          await fetch('http://origin.com/path/param');
+          expect(
+            mswInspector.getRequests('http://origin.com/:param1/:param2'),
+          ).toHaveBeenCalledTimes(1);
+        });
+      });
+
+      describe('less :namedParams then actual paths segments', () => {
+        it('throw expected error', async () => {
+          await fetch('http://origin.com/path/param');
+          expect(() =>
+            mswInspector.getRequests('http://origin.com/:param1/'),
+          ).toThrow(
+            '[msw-inspector] Cannot find a matching requests for path: http://origin.com/:param1/. Intercepted requests paths are:\n\nhttp://origin.com',
+          );
+        });
+      });
+
+      describe('wildcard (.*)', () => {
+        it('find matching request', async () => {
+          await fetch('http://origin.com/path/param');
+          expect(
+            mswInspector.getRequests('http://origin.com/(.*)'),
+          ).toHaveBeenCalledTimes(1);
+        });
       });
     });
 
@@ -49,75 +100,44 @@ describe('getRequests', () => {
       });
     });
 
-    describe('multiple :namedParams', () => {
-      it('find matching request', async () => {
-        await fetch('http://origin.com/path/param');
-        expect(
-          mswInspector.getRequests('http://origin.com/:param1/:param2'),
-        ).toHaveBeenCalledTimes(1);
-      });
-    });
-
-    describe('less :namedParams then actual paths segments', () => {
-      it('throw expected error', async () => {
+    describe('requesting a url never called', () => {
+      it('throw debug error', async () => {
         await fetch('http://origin.com/path/param');
         expect(() =>
-          mswInspector.getRequests('http://origin.com/:param1/'),
+          mswInspector.getRequests('http://it.was.never.called'),
         ).toThrow(
-          '[msw-inspector] Cannot find a matching requests for path: http://origin.com/:param1/. Intercepted requests paths are:\n\nhttp://origin.com',
+          '[msw-inspector] Cannot find a matching requests for path: http://it.was.never.called. Intercepted requests paths are:\n\nhttp://origin.com',
         );
       });
-    });
 
-    describe('wildcard (.*)', () => {
-      it('find matching request', async () => {
-        await fetch('http://origin.com/path/param');
-        expect(
-          mswInspector.getRequests('http://origin.com/(.*)'),
-        ).toHaveBeenCalledTimes(1);
+      describe('"debug" option === false', () => {
+        it('returns empty mock', async () => {
+          await fetch('http://origin.com/path/param');
+          expect(
+            mswInspector.getRequests('http://it.was.never.called', {
+              debug: false,
+            }),
+          ).not.toHaveBeenCalled();
+        });
       });
     });
 
-    describe('no matching calls', () => {
-      describe('requesting a url never called', () => {
-        it('throw debug error', async () => {
-          await fetch('http://origin.com/path/param');
+    // @NOTE SKipping locally since it involves external network requests
+    if (process.env.CI) {
+      describe('calling and requesting a url not registered as MSW handler', () => {
+        it('throw expected error', async () => {
+          await fetch(
+            'https://api.github.com/repos/toomuchdesign/msw-inspector',
+          );
           expect(() =>
-            mswInspector.getRequests('http://it.was.never.called'),
+            mswInspector.getRequests(
+              'https://api.github.com/repos/toomuchdesign/msw-inspector',
+            ),
           ).toThrow(
-            '[msw-inspector] Cannot find a matching requests for path: http://it.was.never.called. Intercepted requests paths are:\n\nhttp://origin.com',
+            '[msw-inspector] Cannot find a matching requests for path: https://api.github.com/repos/toomuchdesign/msw-inspector.',
           );
         });
-
-        describe('"debug" option === false', () => {
-          it('returns empty mock', async () => {
-            await fetch('http://origin.com/path/param');
-            expect(
-              mswInspector.getRequests('http://it.was.never.called', {
-                debug: false,
-              }),
-            ).not.toHaveBeenCalled();
-          });
-        });
       });
-
-      // @NOTE SKipping locally since it involves external network requests
-      if (process.env.CI) {
-        describe('calling and requesting a url not registered as MSW handler', () => {
-          it('throw expected error', async () => {
-            await fetch(
-              'https://api.github.com/repos/toomuchdesign/msw-inspector',
-            );
-            expect(() =>
-              mswInspector.getRequests(
-                'https://api.github.com/repos/toomuchdesign/msw-inspector',
-              ),
-            ).toThrow(
-              '[msw-inspector] Cannot find a matching requests for path: https://api.github.com/repos/toomuchdesign/msw-inspector.',
-            );
-          });
-        });
-      }
-    });
+    }
   });
 });
